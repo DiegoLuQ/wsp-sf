e7fa99daf1623ace2d6f653b2c1fcd0e4915d126
DiegoCasa <jdluquelinares@gmail.com>
2023-09-19 23:05:52 -0300

    nuevo numero

--- app/whatsappservice.py
+++ app/whatsappservice.py
@@ -4,7 +4,7 @@ import json
 def SendMessageWhatsapp(data):
     try:
         token = "EAAJnnqzxiNoBO1pgyHiDLa5Kts2IkZAd15OEooEemwqhJ4OxpZAtZBerxU6VDAbTaX1302lbugkvFYwQPZAgW5Vvj0X8dIIfnZBKflFYVpVHjYWgUDKCZAo9tmHzALHTB8cVcAhpWzvNCvOp4hxB55reXjnUYA2OPUac0b8NTwqKGg4peev2VAhwKW8G9nx25k"
-        api_url = "https://graph.facebook.com/v17.0/133381839854669/messages"
+        api_url = "https://graph.facebook.com/v17.0/130513470143872/messages"
         headers = {"Content-Type":"application/json", "Authorization":"Bearer " + token}
         response = requests.post(api_url, data = json.dumps(data), headers = headers)
 
